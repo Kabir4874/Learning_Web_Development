0a96c58c8d990d776486166f7d45536d8b407ab9
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-02 12:05:23 +0600

    add

--- Bootstrap/Bootstrap Projects/Darky/js/script.js
+++ Bootstrap/Bootstrap Projects/Darky/js/script.js
@@ -1,4 +1,14 @@
+const scrollToTop= document.querySelector(".scroll-to-top");
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY> 300){
+        scrollToTop.style.display= "grid";
+    }else{
+        scrollToTop.style.display= "none";
+    }
+})
+
 // Swiper 
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween:24,
     speed: 1000,
