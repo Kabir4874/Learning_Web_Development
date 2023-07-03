8a5c9c3551281f76e437b4c8225d8b9c7519629a
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-03 10:40:06 +0600

    add

--- React/CWH/my-app/src/components/TextForm.js
+++ React/CWH/my-app/src/components/TextForm.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("Uppercase was clicked");
+    // console.log("Uppercase was clicked");
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLowClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toLowerCase();
     setText(newText);
   };
 
   const handleOnChange = (event) => {
-    console.log("On Change Handler");
+    // console.log("On Change Handler");
     setText(event.target.value);
   };
 
   const [text, setText] = useState("Enter text here");
   // setText("new text");
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
@@ -29,6 +35,17 @@ export default function TextForm(props) {
       <button className="btn btn-primary" onClick={handleUpClick}>
         Convert to Uppercase
       </button>
+      <button className="btn btn-primary" onClick={handleLowClick}>
+        Convert to Lowercase
+      </button>
     </div>
+    <div className="container my-4">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} Words, {text.length} Characters</p>
+        <p>{0.008* text.split(" ").length} Minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   );
 }
