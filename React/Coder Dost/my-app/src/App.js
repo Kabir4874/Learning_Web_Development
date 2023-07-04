4477cebba5546ee5ba2595fa6e47622ab8e8d900
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-04 17:55:54 +0600

    add

--- React/Coder Dost/my-app/src/App.js
+++ React/Coder Dost/my-app/src/App.js
@@ -2,14 +2,27 @@ import "./App.css";
 import Video from "./Components/Video";
 
 function App() {
+  let obj = {
+    title: "React JS Tutorial",
+    views: "10K",
+    time: "1 Year Ago",
+    channel: "Coder Dost",
+    verified: true
+  };
+
   return (
     <>
-
-      <div>Hello</div>
-
-      <Video title="React Js tutorial" bgColor="red"/>
-      <Video title="Node Js tutorial" bgColor="green"/>
-
+      <div className="App">
+        <div>Videos</div>
+        
+        <Video {...obj} />
+        <Video
+          title="Node Js tutorial"
+          views="15K"
+          time={"1 month ago"}
+          channel={"Code with harry"}
+        />
+      </div>
     </>
   );
 }
--- React/Coder Dost/my-app/src/Components/Video.js
+++ React/Coder Dost/my-app/src/Components/Video.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./Video.css";
 
-function Video({ title, channel, views, time }) {
-  let bg = "dark";
+function Video({ title, channel, views, verified, time }) {
   return (
     <>
-      <div className="pic">
-        <img src="https://source.unsplash.com/500x300/?portrait" alt="" />
+      <div className="container">
+        <div className="pic">
+          <img src="https://source.unsplash.com/500x300/?portrait" alt="" />
+        </div>
+        <div className="title">{title}</div>
+        <div className="channel">
+          {channel}
+          {verified ? "✅" : ""}
+        </div>
+        <div className="views">
+          {views} views <span>.</span>
+          {time}
+        </div>
       </div>
-      <div className="title">{title}</div>
-      <div className="channel">{channel}</div>
-      <div className="views">{views} views <span>.</span>{time}</div>
     </>
   );
 }
