3fc7b5f229f4083bbdc4a18f28b37a66ca1bf1e3
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-04 18:19:47 +0600

    add

--- React/Coder Dost/my-app/src/App.js
+++ React/Coder Dost/my-app/src/App.js
@@ -2,26 +2,42 @@ import "./App.css";
 import Video from "./Components/Video";
 
 function App() {
-  let obj = {
-    title: "React JS Tutorial",
-    views: "10K",
-    time: "1 Year Ago",
-    channel: "Coder Dost",
-    verified: true
-  };
+  let videos = [
+    {
+      title: "React JS Tutorial",
+      views: "10K",
+      time: "1 Year Ago",
+      channel: "Coder Dost",
+      verified: true,
+    },
+    {
+      title: "Node JS Tutorial",
+      views: "15K",
+      time: "1 month Ago",
+      channel: "Code with harry",
+      verified: false,
+    },
+    {
+      title: "MongoDB Tutorial",
+      views: "25K",
+      time: "1 day Ago",
+      channel: "CodeBabbar",
+      verified: true,
+    },
+  ];
 
   return (
     <>
       <div className="App">
         <div>Videos</div>
-        
-        <Video {...obj} />
-        <Video
-          title="Node Js tutorial"
-          views="15K"
-          time={"1 month ago"}
-          channel={"Code with harry"}
-        />
+        {videos.map((video) => (
+          <Video
+            title={video.title}
+            views={video.views}
+            time={video.time}
+            channel={video.channel}
+          />
+        ))}
       </div>
     </>
   );
