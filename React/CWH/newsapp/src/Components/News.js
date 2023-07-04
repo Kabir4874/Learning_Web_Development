4494fd8af1a8aa9c99d481b9493fa5ff4c10a97d
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-04 11:41:56 +0600

    add

--- React/CWH/newsapp/src/Components/News.js
+++ React/CWH/newsapp/src/Components/News.js
@@ -2,18 +2,11 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-
-
-
-  constructor(){ 
+  constructor() {
     super();
     console.log("Hello I am a constructor");
-    this.state={
-
-    }
-   }
-
-
+    this.state = {};
+  }
 
   render() {
     return (
@@ -21,7 +14,11 @@ export class News extends Component {
         <h2>NewsMonkey- Top Headlines</h2>
         <div className="row mt-5">
           <div className="col-md-4">
-            <NewsItem title="My Title" description="MyDesc" imageUrl="https://i.ytimg.com/vi/uKvNfnwzXMA/maxresdefault.jpg" />
+            <NewsItem
+              title="My Title"
+              description="MyDesc"
+              imageUrl="https://i.ytimg.com/vi/uKvNfnwzXMA/maxresdefault.jpg"
+            />
           </div>
           <div className="col-md-4">
             <NewsItem />
---
+++ React/Coder Dost/my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Video from "./Components/Video";
+
+function App() {
+  return (
+    <>
+
+      <div>Hello</div>
+
+      <Video/>
+
+    </>
+  );
+}
+
+export default App;
