3e749da5d098f73439bde90598d98d78abe088a0
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-06 23:26:21 +0600

    add

---
+++ React/React Projects/portfolio/src/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Section from '../intro/shared/section'
+import Logo from '../../images/logo.png'
+import './style.scss'
+import { FaYoutube,FaGithub,FaLinkedinIn,FaStackOverflow, FaInstagram } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
