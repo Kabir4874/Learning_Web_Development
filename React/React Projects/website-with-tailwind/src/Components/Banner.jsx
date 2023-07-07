f95c9b41d3e59a95d325844f95e6117b8293f787
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-08 00:37:09 +0600

    add

--- React/React Projects/website-with-tailwind/src/Components/Banner.jsx
+++ React/React Projects/website-with-tailwind/src/Components/Banner.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import Typed from "react-typed";
 
 const Banner = () => {
   return (
-    <div className=' bg-[#2699fb] w-full py-[100px] '>
-      <div className=' max-w-[1240px] mx-auto text-center'>
-        <div>Learn with us</div>
-        <h2 className=' text-white font-bold text-[100px]'>Grow with us</h2>
-        <div>Learn</div>
+    <div className=" bg-[#2699fb] w-full py-[50px] ">
+      <div className=" max-w-[1240px] mx-auto text-center font-bold py-[100px]">
+        <div className="text-xl md:text-3xl md:p-[24px] ">Learn with us</div>
+        <h2 className=" text-white text-4xl my-5 md:p-[24px]   md:text-[80px]">
+          Grow with us.
+        </h2>
+        <div className="md:text-[50px] text-2xl text-white  md:p-[24px]">
+          Learn
+          <Typed
+            className=" pl-3"
+            strings={["Web Development", "UI/UX Design", "Web Design"]}
+            typeSpeed={100}
+            backSpeed={50}
+            loop
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
