1966ce877d727a8a5358b63dd2a6751bf4f29b93
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-12 22:21:06 +0600

    add

--- React/React Projects/Small Projects/3.dice_game/src/Components/Dice.jsx
+++ React/React Projects/Small Projects/3.dice_game/src/Components/Dice.jsx
@@ -1,9 +1,37 @@
-
+import { useState } from "react";
+import { styled } from "styled-components";
 
 const Dice = () => {
-  return (
-    <div>Dice</div>
-  )
-}
+  const [currentDice, setCurrentDice] = useState();
 
-export default Dice
+  const generateRandomNumber = (min, max) => {
+    console.log(Math.floor(Math.random() * (max - min) + min));
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={() => generateRandomNumber(1, 6)}>
+        <img src="../../public/images/dice/dice_1.png" alt="" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+export default Dice;
+
+const DiceContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 48px;
+  flex-direction: column;
+  align-items: center;
+  .dice {
+    cursor: pointer;
+  }
+  p {
+    font-size: 24px;
+    font-weight: 700;
+  }
+`;
