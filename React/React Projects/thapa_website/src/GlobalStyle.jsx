2201fee2c12da5383bc2524e34c53c754046c518
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-24 21:26:47 +0600

    add

---
+++ React/React Projects/thapa_website/src/GlobalStyle.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Globalstyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Work Sans', sans-serif;
+}
+html{
+    font-size: 62.5%;
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+}
+
+`;
