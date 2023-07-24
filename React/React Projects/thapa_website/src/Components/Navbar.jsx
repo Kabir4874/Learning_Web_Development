fb98f1d98028d1eb02a5c2d760c5ee4da8a18c3e
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-24 22:05:39 +0600

    add

--- React/React Projects/thapa_website/src/Components/Navbar.jsx
+++ React/React Projects/thapa_website/src/Components/Navbar.jsx
@@ -39,12 +39,10 @@ const Nav = styled.nav`
     display: flex;
     gap: 4.8rem;
     li {
-      list-style: none;
       .navbar-link {
         &:link,
         &:visited {
           display: inline-block;
-          text-decoration: none;
           font-size: 1.8rem;
           text-transform: uppercase;
           color: ${({ theme }) => theme.colors.black};
