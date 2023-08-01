f721b740d12fd1cf2f1aab984a838366e334b4fe
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-01 18:52:46 +0600

    add

--- React/React Projects/shopping_cart/src/Pages/Cart/CartItem.jsx
+++ React/React Projects/shopping_cart/src/Pages/Cart/CartItem.jsx
@@ -1,14 +1,17 @@
-const CartItem = ({data}) => {
+const CartItem = ({ data }) => {
   const { id, productName, price, productImage } = data;
   return (
     <div className="cartItem">
       <img src={productImage} alt="" />
       <div className="description">
-            <p><b>{productName}</b></p>
-            <p>${price}</p>
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
       </div>
     </div>
   );
 };
 
 export default CartItem;
+ 
