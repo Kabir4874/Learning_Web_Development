7887822a0815b11084fc4005d1a250d72daf1789
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-12 00:15:58 +0600

    add

--- React/React Projects/greatstack_portfolio/src/App.js
+++ React/React Projects/greatstack_portfolio/src/App.js
@@ -1,9 +1,11 @@
+import Intro from "./Components/Intro/Intro";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
