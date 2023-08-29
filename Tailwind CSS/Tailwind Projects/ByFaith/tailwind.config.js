458a905d86707eec180929049a05533b9d29c143
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-29 22:10:27 +0600

    add

---
+++ Tailwind CSS/Tailwind Projects/ByFaith/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: " #f79918",
+        secondary: "#10495c",
+        textColor: "#888",
+        footerColor: "#ede1d0",
+      },
+    },
+  },
+  plugins: [],
+};
