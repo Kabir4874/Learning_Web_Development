a9d60bdd1bc5a3dba38e473ed16adcb8eea2cc18
Kabir4874 <kabir.cse.bd@gmail.com>
2023-12-18 19:56:16 +0600

    add

---
+++ Tailwind CSS/Tailwind Projects/Discord Clone/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepBlue: "#404EED",
+        lightWhite: "#f6f6f6",
+        lightBlue: "#5865f2",
+        darkBg: "#23272a",
+      },
+      fontFamily:{
+        whitney:["Whitney"],
+        ginto:["Ginto"],
+        ggSans:["ggSans"],
+      },
+    },
+  },
+  plugins: [],
+}
+
