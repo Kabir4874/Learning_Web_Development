177e8008f3eec5b3a00e1d8994f878ee1917d1e1
Kabir4874 <kabir.cse.bd@gmail.com>
2024-01-17 21:11:51 +0600

    add

--- Practice Project/BREED/script.js
+++ Practice Project/BREED/script.js
@@ -1,9 +1,8 @@
-const nav= document.querySelector(".nav-wrapper");
-window.addEventListener('scroll',()=>{
-    if (window.scrollY > 60) {
-        nav.classList.add("scrolled");
-      } else {
-        nav.classList.remove("scrolled");
-      }
-})
-
+const nav = document.querySelector(".nav-wrapper");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
