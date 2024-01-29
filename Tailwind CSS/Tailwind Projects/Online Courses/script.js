38bf7cab920dd00941866be8944cd97959cd6c5f
Kabir4874 <kabir.cse.bd@gmail.com>
2024-01-29 23:16:43 +0600

    add

--- Tailwind CSS/Tailwind Projects/Online Courses/script.js
+++ Tailwind CSS/Tailwind Projects/Online Courses/script.js
@@ -6,24 +6,24 @@ window.addEventListener("scroll", () => {
   if (scrollY > 40) {
     topBanner.classList.add("top-banner");
     navbar.classList.add("navbar");
-    navbar.style.marginTop="0"
+    navbar.style.marginTop = "0";
   } else {
     topBanner.classList.remove("top-banner");
     navbar.classList.remove("navbar");
+    navbar.style.marginTop = "5rem";
   }
 });
 
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const cross = document.getElementById("cross");
 
-const hamburger= document.getElementById("hamburger")
-const menu= document.getElementById("menu")
-const cross= document.getElementById("cross")
+hamburger.addEventListener("click", () => {
+  menu.style.left = "0";
+  cross.classList.remove("hidden");
+  cross.style.display = "block";
+});
 
-hamburger.addEventListener("click", ()=>{
-  menu.style.left="0"
-  cross.classList.remove("hidden")
-  cross.style.display="block"
-})
-
-cross.addEventListener("click", ()=>{
-  menu.style.left="105%"
-})
+cross.addEventListener("click", () => {
+  menu.style.left = "105%";
+});
