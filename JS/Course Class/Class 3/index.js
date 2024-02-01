36651c6a1a808ede2d0ca76061dbe74138c9af2f
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-01 18:49:44 +0600

    add

--- JS/Course Class/Class 3/index.js
+++ JS/Course Class/Class 3/index.js
@@ -1,19 +1,54 @@
-// !Strings 
+// !Strings
 
-let lastName= "Ridoy";
-let firstName= new String("Kabir")
-console.log(typeof(firstName));
+// let lastName = "Ridoy";
+// let firstName = new String("Kabir");
+// console.log(typeof firstName);
 
-// !Array 
+// !Array
 
-let number= [1,4,5,7]
-number.push(5)
-number.unshift(6)
-number.splice(2,0,"kabir")
-console.log(number);
+// let number = [1, 4, 5, 7];
+// number.push(5);
+// number.unshift(6);
+// number.splice(2, 0, "kabir");
+// console.log(number);
 
-let courses=[
-    {no:1, nam: 'kabir'},
-    {no:2, nam: 'ahmed'}
-]
-console.log(courses);
+// let courses = [
+//   { no: 1, nam: "kabir" },
+//   { no: 2, nam: "ahmed" },
+// ];
+// console.log(courses);
+
+// let course = courses.find(function (course) {
+//   return course.nam == "kabir";
+// });
+
+// console.log(course);
+// number.length = 0;
+// console.log(number);
+
+// let first= [1,2,3]
+// let second= [4,5,7]
+// let combined= first.concat(second)
+// console.log(combined);
+
+// let sliced = combined.slice(2,4)
+// console.log(sliced);
+// console.log("Hello");
+
+// let com= [...first,...second,'a','b']
+// console.log(com);
+
+// let arr = [1, 2, 3, 4, 5];
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// arr.forEach((number) => console.log(number));
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+// const joined = numbers.join(",");
+// console.log(joined);
+
+let message='This is my first message';
+let parts= message.split(' ');
+console.log(parts);
