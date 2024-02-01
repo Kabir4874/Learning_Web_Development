1b95dc988e9aca70ac23a8ccb214ac81dff78b79
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-01 15:54:02 +0600

    add

--- JS/Course Class/Class 2/index.js
+++ JS/Course Class/Class 2/index.js
@@ -1,13 +1,40 @@
 console.log("Lets Start");
 
 // object Creation
+// function createRectangle(length,breadth) {
+//   return (rectangle = {
+//     length,
+//     breadth,
+//     draw: function () {
+//       console.log("Drawing Rectangle");
+//     },
+//   });
+// }
+
+// let obj1 = createRectangle(2,3);
+
+// obj1.draw();
+// console.log(obj1.breadth);
+
+// !Constructor Function
+
+// function rectangle() {
+//   this.length = 1;
+//   this.breadth = 2;
+//   this.draw = function () {
+//     console.log("Drawing");
+//   };
+// }
+
+// let rectangleObj= new rectangle();
 
 let rectangle = {
-  length: 1,
-  breadth: 2,
-  draw: function(){
-    console.log("Draw");
-  }
+  side: 2,
+  breadth: 4,
 };
 
-rectangle.draw()
+// !For in loop
+
+for (let key in rectangle) {
+  console.log(key, rectangle[key]);
+}
