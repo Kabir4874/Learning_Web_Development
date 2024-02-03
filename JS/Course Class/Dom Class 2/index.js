9365acd9c6b8fab7eb856bee6daa703e18b0d5aa
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-03 22:55:53 +0600

    add

--- JS/Course Class/Dom Class 2/index.js
+++ JS/Course Class/Dom Class 2/index.js
@@ -4,3 +4,30 @@ function eventFunction() {
 
 document.addEventListener("click", eventFunction);
 document.removeEventListener("click", eventFunction);
+
+const content = document.querySelector("#wrapper");
+content.addEventListener("click", function (event) {
+  console.log(event);
+});
+
+let links = document.querySelectorAll("a");
+
+let thirdLink = links[2];
+
+thirdLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Chill");
+});
+
+let myDiv = document.createElement("div");
+
+function paraStatus(event) {
+  console.log(event.target.textContent);
+}
+myDiv.addEventListener("click", paraStatus);
+for (let i = 1; i <= 100; i++) {
+  let newElement = document.createElement("p");
+  newElement.textContent = "This is pera" + i;
+  myDiv.appendChild(newElement);
+}
+document.body.appendChild(myDiv);
