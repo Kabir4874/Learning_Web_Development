17c50b78b83593d4fca520e556a3de5237064a1e
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-04 19:12:25 +0600

    add

--- JS/Course Class/Dom Class 4/index.js
+++ JS/Course Class/Dom Class 4/index.js
@@ -8,18 +8,39 @@
 // sync();
 // console.log("Second");
 
-let promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("I am inside Promise");
-  }, 5000);
-  // resolve(1998);
-  reject(new Error("Error Occurred"));
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     console.log("I am inside Promise");
+//   }, 5000);
+// resolve(1998);
+//   reject(new Error("Error Occurred"));
+// });
+
+// promise.then((value) => {
+//   console.log(value);
+// });
+// promise.catch((error) => {
+//   console.log(error);
+// });
+// console.log("First");
+
+let wada1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("setTimeOut1 Started");
+  }, 2000);
+  resolve(true);
 });
 
-promise.then((value) => {
+let output = wada1.then(() => {
+  let wada2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      console.log("setTimeOut2 Started");
+    }, 3000);
+    resolve("Wada 2 resolved");
+  });
+  return wada2;
+});
+
+output.then((value) => {
   console.log(value);
 });
-promise.catch((error) => {
-  console.log(error);
-});
-console.log("First");
