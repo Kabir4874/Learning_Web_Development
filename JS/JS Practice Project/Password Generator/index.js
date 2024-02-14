f34b3e3d053ab7eb1c4b76978b9a87c69a0d8d05
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-14 22:11:08 +0600

    add

--- JS/JS Practice Project/Password Generator/index.js
+++ JS/JS Practice Project/Password Generator/index.js
@@ -33,11 +33,16 @@ setIndicator("#ccc");
 function handleSlider() {
   inputSlider.value = passwordLength;
   lengthDisplay.innerText = passwordLength;
+  const min = inputSlider.min;
+  const max = inputSlider.max;
+  inputSlider.style.backgroundSize =
+    ((passwordLength - min) * 100) / (max - min) + "% 100%";
 }
 handleSlider();
 
 function setIndicator(color) {
   indicator.style.backgroundColor = color;
+  indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRandomInt(min, max) {
