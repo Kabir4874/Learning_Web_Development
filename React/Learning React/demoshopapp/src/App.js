0153ea494028c53258d4dbff4700ba5ea271a3a3
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-21 21:56:22 +0600

    add

--- React/Learning React/demoshopapp/src/App.js
+++ React/Learning React/demoshopapp/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Item from './components/Item';
 
 function App() {
   return (
+    <div>
+    <Item/>
     <div className="App">
      Hello World
+    </div>
     </div>
   );
 }
