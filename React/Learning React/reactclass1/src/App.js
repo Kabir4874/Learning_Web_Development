e78f56af42f632cebdc36bf0d8f207f6de5b8dd4
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-23 18:38:26 +0600

    add

---
+++ React/Learning React/reactclass1/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Item from "./components/Item";
+
+function App() {
+  return (
+    <div>
+      <Item />
+      <div className="App">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
