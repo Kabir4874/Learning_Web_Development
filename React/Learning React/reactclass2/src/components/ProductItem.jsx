25cd61fb8c701a15a7049dba5413d675bb342d11
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-23 18:47:53 +0600

    add

--- React/Learning React/reactclass2/src/components/ProductItem.jsx
+++ React/Learning React/reactclass2/src/components/ProductItem.jsx
@@ -11,6 +11,7 @@ const ProductItem = (props) => {
         <div className="product-item_description">
           <h2>{props.title}</h2>
         </div>
+        <button>Add to Cart</button>
       </Card>
     </>
   );
