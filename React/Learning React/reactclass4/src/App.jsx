c7417446fc9f42ac8f2e5068684738b6b4cea02d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-26 20:54:48 +0600

    add

--- React/Learning React/reactclass4/src/App.jsx
+++ React/Learning React/reactclass4/src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [name,setName]= useState('Kabir');
+  // !Variation 1 -> Every Render
+  // useEffect(()=>{
+  //   console.log("UI Rendering Done");
+  // });
+  // !Variation 2 -> First Render
+  // useEffect(() => {
+  //   console.log("UI Rendering Done");
+  // }, []);
+  // !Variation 3 -> First Render + Whenever Dependency Changes
+  useEffect(() => {
+    console.log("Change Observed");
+  }, [name]);
+
   function changeHandler(event) {
     console.log(text);
     setText(event.target.value);
