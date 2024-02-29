701c48e7ddd88bdf3fc7a355cc762c75a77e017a
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-29 15:31:45 +0600

    add

--- React/Learning React/reactintermediate-1/src/App.jsx
+++ React/Learning React/reactintermediate-1/src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  console.log(firstName);
-  console.log(lastName);
-  function changeFirstNameHandler(event) {
-    // console.log("Printing First Name:");
-    // console.log(event.target.value);
-    setFirstName(event.target.value);
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // console.log(firstName);
+  // console.log(lastName);
+  // function changeFirstNameHandler(event) {
+  // console.log("Printing First Name:");
+  // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+  // function changeSecondNameHandler(event) {
+  // console.log("Printing Last Name:");
+  // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  console.log(formData.email);
+  function changeHandler(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
-  function changeSecondNameHandler(event) {
-    // console.log("Printing Last Name:");
-    // console.log(event.target.value);
-    setLastName(event.target.value);
-  }
+
   return (
     <div>
       <form>
         <input
           type="text"
           placeholder="First Name"
-          onChange={changeFirstNameHandler}
+          onChange={changeHandler}
+          name="firstName"
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={changeHandler}
+          name="lastName"
         />
         <br />
         <br />
         <input
-          type="text"
-          placeholder="Last Name"
-          onChange={changeSecondNameHandler}
+          type="email"
+          placeholder="Enter Your Email"
+          onChange={changeHandler}
+          name="email"
         />
       </form>
     </div>
