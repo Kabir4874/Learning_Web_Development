65dab3a2254fc0069dc0a8e55f5a7b3d901c45e7
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-29 15:12:12 +0600

    add

--- React/Learning React/reactintermediate-1/src/App.jsx
+++ React/Learning React/reactintermediate-1/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  console.log(firstName);
+  console.log(lastName);
+  function changeFirstNameHandler(event) {
+    // console.log("Printing First Name:");
+    // console.log(event.target.value);
+    setFirstName(event.target.value);
+  }
+  function changeSecondNameHandler(event) {
+    // console.log("Printing Last Name:");
+    // console.log(event.target.value);
+    setLastName(event.target.value);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={changeFirstNameHandler}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={changeSecondNameHandler}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
