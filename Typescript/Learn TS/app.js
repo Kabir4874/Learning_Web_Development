52bd8b522512be9632355738889918544374d8e2
kabir4874 <kabir.cse.bd@gmail.com>
2024-10-02 22:32:27 +0600

    update

---
+++ Typescript/Learn TS/app.js
@@ -0,0 +1,35 @@
+"use strict";
+// let array: [string, number] = ["Harsh", 22];
+// enum UserRoles {
+//   ADMIN = "admin",
+//   GUEST = "guest",
+//   SUPER_ADMIN = "super_admin",
+// }
+// let a:number;
+// a=13;
+//! interface types
+// interface User {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
+// interface Admin extends User{
+//     admin:boolean;
+// }
+// function abcd(obj:Admin){
+// }
+//! type alias
+// type value = number | number | null;
+// let a: value;
+// !class and objects
+// class Device {
+//   constructor(public name: string, public readonly price: number) {}
+//   get deviceName() {
+//     return this.name;
+//   }
+//   set deviceName(value: string) {
+//     this.name = value;
+//   }
+// }
+// let d1 = new Device("Iphone", 232344324);
+// !generics 
--- Typescript/index.js
+++
@@ -1,8 +0,0 @@
-// console.log('I am kabir ahmed ridoy');
-// let num:number= 5;
-// console.log(num);
-//! function define
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(5, 10));
